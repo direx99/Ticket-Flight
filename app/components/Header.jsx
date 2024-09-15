d54b2ce8a40237cb27c884a898011dab9978143f
Dinith Perera <dinith@team.bhasha.lk>
2024-09-15 22:53:33 +0530

    Project init.

---
+++ app/components/Header.jsx
@@ -0,0 +1,16 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const Header = () => {
+  return (
+    <View>
+      {/* Left button */}
+      {/* Header Content */}
+      {/* Right Button */}
+    </View>
+  );
+};
+
+export default Header;
+
+const styles = StyleSheet.create({});
