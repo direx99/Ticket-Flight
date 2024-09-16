7f19684495af6bf33ba87e52c7b11b87f866dd70
Dinith Perera <dinith@team.bhasha.lk>
2024-09-16 23:59:42 +0530

    Typo changes.

--- app/screens/Home.jsx
+++ app/screens/Home.jsx
@@ -6,7 +6,14 @@ const Home = () => {
   return (
     <SafeAreaView style={styles.container}>
       <Header />
-      <Text>Home</Text>
+      <View style={styles.contentArea}>
+        <View>
+          <Text style={styles.largeTitleText}>Discover a new place.</Text>
+          <Text style={styles.largeSecondaryText}>
+            Explore, Journey, Discover & Adventure.
+          </Text>
+        </View>
+      </View>
     </SafeAreaView>
   );
 };
@@ -16,8 +23,23 @@ export default Home;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    width: "100%",
+    backgroundColor: "#080808",
     alignItems: "center",
     justifyContent: "center",
+  },
+  contentArea: {
+    flex: 1,
+    width: "100%",
+    padding: 20,
+  },
+  largeTitleText: {
+    color: "#ffffff",
+    fontSize: 28,
+  },
+  largeSecondaryText: {
+    color: "#ffffff",
+    fontSize: 15,
+    opacity: 0.5,
   },
 });
